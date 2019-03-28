e39182a8d7e2f04f7bfd3480bb2267639654bd66
Viktor Zetterström <viktorzetterstrom@gmail.com>
2019-03-28 16:31:35 +0100

    add day 1 solution

---
+++ 1/cronal-calibration.js
@@ -0,0 +1,41 @@
+// PART 1
+// Read input from file
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().split('\n');
+
+// Convert contents to numbers
+numbers = input.map(value => parseInt(value));
+
+// Loop over input to get frequency
+let frequency = numbers.reduce((value, sum) => value + sum);
+
+// Print frequency
+console.log("Part 1: " + frequency);
+
+
+// PART 2
+// Use set to store past frequencies
+let frequencies = new Set();
+
+currentFrequency = 0;
+frequencies.add(currentFrequency);
+
+// Loop through input, see if frequency exists in set, otherwise add it
+let duplicateFound = false;
+let index = 0;
+while (!duplicateFound) {
+  const number = numbers[index % numbers.length];
+
+  currentFrequency += number;
+
+  if (frequencies.has(currentFrequency)) {
+    break;
+  } else {
+    frequencies.add(currentFrequency);
+  }
+
+  index ++;
+}
+
+// Print first duplicate frequency
+console.log("Part 2: " + currentFrequency);
---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code 2018
+These are my solutions to the Advent of Code 2018, using Javascript as the solving language.
